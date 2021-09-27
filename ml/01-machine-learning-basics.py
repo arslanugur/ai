1aa4144b652728e5515c151d9360d46fa53f7070
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-09-27 10:34:59 +0300

    Create 01-machine-learning-basics.py

---
+++ ml/01-machine-learning-basics.py
@@ -0,0 +1,58 @@
+Machine Learning Basics
+      #To use several Python packages that are helpful in solving Machine Learning problems. 
+      #Pandas is used for reading data and data manipulation, 
+      #numpy is used for computations of numerical data, 
+      #matplotlib is used for graphing data, and scikit-learn is used for machine learning models. 
+
+      #Supervised learning is when we have a known target based on past data (for example, predicting what price a house will sell for)
+      #deal with Prediction
+      #Unsupervised learning is when there isn't a known past answer (for example, determining the topics discussed in restaurant reviews)
+      #deal with Deduction
+      
+      #Supervised Learning
+      #Within supervised learning, there are classification and regression problems. 
+      #Regression is predicting a numerical value (for example, predicting what price a house will sell for)
+      #Classification is predicting what class something belongs to (for example, predicting if a borrower will default on their loan)
+ 
+"""      
+      Classification problems where we’re predicting which class something belongs to.
+      Our examples will include:
+      • Predicting who would survive the Titanic crash
+      • Determining a handwritten digit from an image
+      • Using biopsy data to classify if a lump is cancerous
+
+      To use a number of popular techniques to tackle these problems.
+      • Logistic Regression
+      • Decision Trees
+      • Random Forests
+      • Neural Networks
+"""
+
+      #Unsupervised learning : Clustering problem, Non-clustering problem. 
+      #Regression problem examples
+      #Prediction of continuous data
+      #Target variables that are of continuous values. In other words, numerical data (-inf to +inf, percentage, etc.)
+
+      
+      #Classification problem examples
+        #Target variables that are of discrete values. In other words, categorical data (yes/no, 1/0, etc.)
+        #Prediction of limited number of outputs. Like yes/no
+        #Determining if an image is of a car, bus or bike
+        #Predicting if a credit card charge is fraudulent  
+      
+
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      

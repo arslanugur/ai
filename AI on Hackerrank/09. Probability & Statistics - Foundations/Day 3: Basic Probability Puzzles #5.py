2f93ebdb2df75c6b6c2d960445723113fd4ea705
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 23:07:24 +0300

    Create Day 3: Basic Probability Puzzles #5.py

---
+++ AI on Hackerrank/09. Probability & Statistics - Foundations/Day 3: Basic Probability Puzzles #5.py
@@ -0,0 +1,10 @@
+# Challenge: https://www.hackerrank.com/challenges/basic-probability-puzzles-5
+
+# Reference: https://en.wikipedia.org/wiki/Probability
+
+# One person will sit, leaving nine positions. 
+# Each position has two seats, one on the left side and one on the right side. 
+
+# Result = 0.222222222 = 2/9
+print (2/9)
+

ace6cf2b7786dad89224f890461b8f676320b36b
Arslan <45125597+arslanugur@users.noreply.github.com>
2021-11-02 23:07:48 +0300

    Create Day 3: Basic Probability Puzzles #6.py

---
+++ AI on Hackerrank/09. Probability & Statistics - Foundations/Day 3: Basic Probability Puzzles #6.py
@@ -0,0 +1,9 @@
+# Challenge: https://www.hackerrank.com/challenges/basic-probability-puzzles-6
+
+# Reference: https://en.wikipedia.org/wiki/Probability
+
+# P(Y = b | X = w) * P(X = w) + P(Y = b | X = b) + P(X = b)
+# (6/14 * 5/9) + (7/14 * 4/9) = 29/63 = 0.46031746
+
+print ((6/14 * 5/9) + (7/14 * 4/9))
+
